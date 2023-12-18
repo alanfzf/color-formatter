a96f346a8396c96e1ad62677b7de377b733aca2b
alanfvn <alandavidg16@gmail.com>
2023-12-17 22:58:17 -0600

    Updated script

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
+import re
 
-def procesar_archivos(archivo_a, archivo_b, salida_a, salida_b):
+def process_files(archivo_a, archivo_b, salida_a, salida_b, inverse=False):
     with open(archivo_a, 'r') as f1, open(archivo_b, 'r') as f2, open(salida_a, 'w') as s1, open(salida_b, 'w') as s2:
         lineas_a = f1.readlines()
         lineas_b = f2.readlines()
@@ -21,14 +22,28 @@ def procesar_archivos(archivo_a, archivo_b, salida_a, salida_b):
                     break
             else:
                 coincidencias_a.append(linea_a)
+                # if i want an inverse search use this: 
+                # coincidencias_b.append(linea_a)
 
         s1.writelines(coincidencias_a)
         s2.writelines(coincidencias_b + lineas_b)
 
-if __name__ == '__main__':
-    archivo_a = 'list1.txt'  # Reemplaza con el nombre de tu primer archivo
-    archivo_b = 'list2.txt'  # Reemplaza con el nombre de tu segundo archivo
-    salida_a = 'salida_a.txt'    # Nombre del primer archivo de salida
-    salida_b = 'salida_b.txt'    # Nombre del segundo archivo de salida
+def get_hex_values(input_file, output_file):
+    patron_hex = re.compile(r'#(?:[0-9a-fA-F]{3}){1,2}\b')
+    hex_colors = set()
 
-    procesar_archivos(archivo_a, archivo_b, salida_a, salida_b)
+    with open(input_file, 'r') as f:
+        contenido = f.read()
+        hex_colors.update(patron_hex.findall(contenido))
+
+    with open(output_file, 'w') as o:
+        # o.writelines(color + '\n' for color in hex_colors)
+        o.writelines(hex_colors)
+
+if __name__ == '__main__':
+    # file A: contains the base, file B: contains the new inputs
+    file_a, file_b  = 'list1.txt','list2.txt'
+    output_a, output_b = 'out1.txt', 'out2.txt'
+    output_c = 'hex.txt'
+    process_files(file_a, file_b, output_a, output_b)
+    get_hex_values(file_b, output_c)
